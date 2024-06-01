ad82c4cfc3c03607c55a7ad3dc46713e8235c7c7
Стас Цветков <schvetkov@dvfu.ru>
2024-06-01 12:15:15 +1000

    Initial commut

---
+++ src/test/java/org/jenjetsu/graduate_project/GraduateProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jenjetsu.graduate_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduateProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
